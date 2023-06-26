9358e172b7258a4a93bf2a231e575e1616c96294
Parth Desai <parthdesai020@gmail.com>
2023-06-26 18:02:32 +0530

    Dashboard UI

--- Endevours Web/dashboard/Admin/assets/demo/demo.js
+++ Endevours Web/dashboard/Admin/assets/demo/demo.js
@@ -204,6 +204,54 @@ demo = {
       }
     };
 
+    gradientChartOptionsConfigurationWithTooltipEndeavoursGreen = {
+      maintainAspectRatio: false,
+      legend: {
+        display: false
+      },
+
+      tooltips: {
+        backgroundColor: '#f5f5f5',
+        titleFontColor: '#333',
+        bodyFontColor: '#666',
+        bodySpacing: 4,
+        xPadding: 12,
+        mode: "nearest",
+        intersect: 0,
+        position: "nearest"
+      },
+      responsive: true,
+      scales: {
+        yAxes: [{
+          barPercentage: 1.6,
+          gridLines: {
+            drawBorder: false,
+            color: 'rgba(0,172,83,0.0)',
+            zeroLineColor: "transparent",
+          },
+          ticks: {
+            suggestedMin: 60,
+            suggestedMax: 125,
+            padding: 20,
+            fontColor: "#9a9a9a"
+          }
+        }],
+
+        xAxes: [{
+          barPercentage: 1.6,
+          gridLines: {
+            drawBorder: false,
+            color: 'rgba(0,172,83,0.2)',
+            zeroLineColor: "transparent",
+          },
+          ticks: {
+            padding: 20,
+            fontColor: "#00ac53"
+          }
+        }]
+      }
+    };
+
     gradientChartOptionsConfigurationWithTooltipOrange = {
       maintainAspectRatio: false,
       legend: {
@@ -431,9 +479,9 @@ demo = {
 
     var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
 
-    gradientStroke.addColorStop(1, 'rgba(72,72,176,0.1)');
-    gradientStroke.addColorStop(0.4, 'rgba(72,72,176,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(119,52,169,0)'); //purple colors
+    gradientStroke.addColorStop(1, 'rgba(0, 172, 83, 0.2)');
+    gradientStroke.addColorStop(0.4, 'rgba(0, 172, 83, 0.0)');
+    gradientStroke.addColorStop(0, 'rgba(0, 172, 83, 0)'); //purple colors
     var config = {
       type: 'line',
       data: {
@@ -442,13 +490,13 @@ demo = {
           label: "My First dataset",
           fill: true,
           backgroundColor: gradientStroke,
-          borderColor: '#d346b1',
+          borderColor: '#00ac53',
           borderWidth: 2,
           borderDash: [],
           borderDashOffset: 0.0,
-          pointBackgroundColor: '#d346b1',
+          pointBackgroundColor: '#00ac53',
           pointBorderColor: 'rgba(255,255,255,0)',
-          pointHoverBackgroundColor: '#d346b1',
+          pointHoverBackgroundColor: '#00ac53',
           pointBorderWidth: 20,
           pointHoverRadius: 4,
           pointHoverBorderWidth: 15,
@@ -456,7 +504,7 @@ demo = {
           data: chart_data,
         }]
       },
-      options: gradientChartOptionsConfigurationWithTooltipPurple
+      options: gradientChartOptionsConfigurationWithTooltipEndeavoursGreen
     };
     var myChartData = new Chart(ctx, config);
     $("#0").click(function() {
